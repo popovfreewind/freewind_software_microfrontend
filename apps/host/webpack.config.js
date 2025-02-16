2691aa14fd74edcb9e48f2214106d108cd907b0a
Dmytro <popovfreewind@gmail.com>
2025-02-16 14:16:31 +0100

    Add Babel loader configuration for JavaScript and JSX files in webpack

--- apps/host/webpack.config.js
+++ apps/host/webpack.config.js
@@ -20,6 +20,16 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
         ],
     },
     plugins: [
@@ -31,8 +41,8 @@ module.exports = {
                     portfolioReact: 'portfolioReact@http://localhost:3001/remoteEntry.js',
                 },
                 shared: {
-                    react: { singleton: true, eager: true  },
-                    'react-dom': { singleton: true, eager: true  },
+                    react: { singleton: true, eager: true },
+                    'react-dom': { singleton: true, eager: true },
                 },
             }
         )
