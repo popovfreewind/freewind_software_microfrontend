bb512300b38d6735828617450295d850a8711ffe
Dmytro <popovfreewind@gmail.com>
2025-02-18 16:55:47 +0100

    Refactor webpack configuration for dynamic environment handling and improve output paths

--- apps/host-app/webpack.config.js
+++ apps/host-app/webpack.config.js
@@ -2,49 +2,56 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require("webpack").container;
 
-module.exports = {
-    entry: './src/index.js',
-    devtool: 'source-map',
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 3000,
-    },
-    output: {
-        publicPath: "http://localhost:3000/",
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: './src/index.js',
+        mode: argv.mode,
+        devtool: 'source-map',
+        devServer: {
+            static: path.resolve(__dirname, 'dist'),
+            port: 3000,
+        },
+        output: {
+            publicPath: isProduction ? './dist/' : 'http://localhost:3000/',
+            path: path.resolve(__dirname, '../../dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                        },
                     },
                 },
-            },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: './src/index.html' }),
+            new ModuleFederationPlugin(
+                {
+                    name: 'host',
+                    remotes: {
+                        portfolioReact: isProduction
+                            ? 'portfolioReact@https://popovfreewind.github.io/freewind_software_microfrontend/portfolio-react/remoteEntry.js'
+                            : 'portfolioReact@http://localhost:3001/remoteEntry.js',
+                    },
+                    shared: {
+                        react: { singleton: true, eager: true },
+                        'react-dom': { singleton: true, eager: true },
+                        'mvp.css': { singleton: true, eager: true },
+                    },
+                }
+            ),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html' }),
-        new ModuleFederationPlugin(
-            {
-                name: 'host',
-                remotes: {
-                    portfolioReact: 'portfolioReact@http://localhost:3001/remoteEntry.js',
-                },
-                shared: {
-                    react: { singleton: true, eager: true },
-                    'react-dom': { singleton: true, eager: true },
-                    'mvp.css': { singleton: true, eager: true },
-                },
-            }
-        ),
-    ],
+    }
 };
--- apps/portfolio-react/webpack.config.js
+++ apps/portfolio-react/webpack.config.js
@@ -2,51 +2,55 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require("webpack").container;
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'source-map',
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 3001,
-    },
-    output: {
-        publicPath: "http://localhost:3001/",
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: './src/index.js',
+        mode: argv.mode,
+        devtool: 'source-map',
+        devServer: {
+            static: path.resolve(__dirname, 'dist'),
+            port: 3001,
+        },
+        output: {
+            publicPath: isProduction ? './dist/portfolio-react/' : 'http://localhost:3001/',
+            path: path.resolve(__dirname, '../../dist/portfolio-react'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                        },
                     },
                 },
-            },
+            ],
+        },
+        plugins: [
+            new ModuleFederationPlugin({
+                name: 'portfolioReact',
+                filename: "remoteEntry.js",
+                exposes: {
+                    "./PortfolioComponent": "./src/PortfolioComponent",
+                },
+                shared: {
+                    react: { singleton: true, eager: true },
+                    'react-dom': { singleton: true, eager: true },
+                    'mvp.css': { singleton: true, eager: true },
+                },
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+            }),
         ],
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'portfolioReact',
-            filename: "remoteEntry.js",
-            exposes: {
-                "./PortfolioComponent": "./src/PortfolioComponent",
-            },
-            shared: {
-                react: { singleton: true, eager: true },
-                'react-dom': { singleton: true, eager: true },
-                'mvp.css': { singleton: true, eager: true },
-            },
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-    ],
+    };
 };
